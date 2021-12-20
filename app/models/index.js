3f2315db9d2110da6cebdb233abe3abf22dd2226
manhnguyen015.mnd@gmail.com <manhnguyen015.mnd@gmail.com>
2021-12-20 21:12:50 +0700

    fix bug to run heroku

--- app/models/index.js
+++ app/models/index.js
@@ -6,13 +6,13 @@ const sequelize = new Sequelize(dbConfig.DB, dbConfig.USER, dbConfig.PASSWORD, {
   host: dbConfig.HOST,
   dialect: dbConfig.dialect,
   operatorsAliases: false,
-  // logging: false,
-  // dialectOptions: {
-  //   ssl: {
-  //     require: true,
-  //     rejectUnauthorized: false,
-  //   }
-  // },
+  logging: false,
+  dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false,
+    }
+  },
 
   pool: {
     max: dbConfig.pool.max,
